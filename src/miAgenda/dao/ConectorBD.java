27e30ddcedb9546ea2fdd2804528cae0c2b2059f
Ben <Ben@Ben>
2025-07-28 12:06:16 -0600

    Commit 3-eliminar los mains en ui y meter codigo en dao

--- src/miAgenda/dao/ConectorBD.java
+++ src/miAgenda/dao/ConectorBD.java
@@ -4,10 +4,26 @@
  */
 package miAgenda.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Ben
  */
 public class ConectorBD {
-    
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=master;encrypt=true;trustServerCertificate=true";
+    private static final String USER = "sa";  // Usuario por default de la BD
+    private static final String PASSWORD = "112306"; // contraseña de la BD
+
+    public static Connection obtenerConexion() throws SQLException {
+        //aviso de algun problema con la BD 
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("No se encontró el driver JDBC", e);
+        }
+    }
 }
